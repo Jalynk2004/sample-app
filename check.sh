4b3d7e3395cbe8a29ff88f3e2b3556c554f0e44b
Jalynk2004 <gialinhnguyen576@gmail.com>
2024-09-30 07:47:23 +0000

    Add check.sh

--- check.sh
+++ check.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
 # Đường dẫn ứng dụng
-APP_URL="http://localhost:5050"
-
-# Gửi yêu cầu HTTP và lưu kết quả
-RESPONSE=$(curl "$APP_URL")
-
-# Kiểm tra nếu thông tin xác nhận thành công có trong phản hồi
-if echo "$RESPONSE" | grep "calling"; then
-    echo "Thành công"
-    exit 0
-else
-    echo "Không thành công"
-    exit 1
-fi
+ip addr
